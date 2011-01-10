8e6fd2a5e4cb3580ed88308ae287b171a627c65f
Travis Vachon <travis.vachon@gmail.com>
2011-01-09 23:26:38 -0500

    v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cdt "1.0.0-SNAPSHOT"
+(defproject lein-cdt "1.0.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
